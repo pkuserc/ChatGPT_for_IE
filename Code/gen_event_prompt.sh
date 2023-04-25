7d005b3c8e28b52c631e663d92d210e6b25ad700
YangYang <yangy@stu.pku.edu.cn>
2023-04-25 11:25:01 +0800

    Add files via upload

---
+++ Code/gen_event_prompt.sh
@@ -0,0 +1,6 @@
+python ./Code/EAE/EAE_E_GenPrompt.py
+python ./Code/EAE/EAE_E+_GenPrompt.py
+python ./Code/ED/ED_E_GenPrompt.py
+python ./Code/ED/ED_E+_GenPrompt.py
+python ./Code/EE/EE_E_GenPrompt.py
+python ./Code/EE/EE_E+_GenPrompt.py
